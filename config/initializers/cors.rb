2e827f8d942223acf62c8aab7f07f0494865ee7a
Sophia Kim <mail@fifikim.com>
2022-06-01 08:08:14 -0400

    test origins config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://143.198.229.107'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
